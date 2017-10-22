4833b00cfaee20e2414eab3542c111d23e55b919
joker893 <dyakuza893@gmail.com>
2017-10-22 22:54:12 +0700

    2
    2

--- modul/rajaongkir.php
+++ modul/rajaongkir.php
@@ -1,5 +1,5 @@
 <?php
-
+//GET PROVINCE////////////////////////////////////////////////////////////////////////////////
 function get_province(){
 $curl = curl_init();
 
@@ -33,6 +33,8 @@ if ($err) {
 }
 
 
+
+//GET CITY////////////////////////////////////////////////////////////////////////////////////
 function get_city($province){
 
 if ($_POST['province']==0) {
@@ -72,9 +74,14 @@ if ($err) {
 if (isset($_POST['province'])) {
         echo get_city($_POST['province']);
     }
-
+	
+	
+	
+//GET COST////////////////////////////////////////////////////////////////////////////////////////
 function get_cost($destination){
-	$curl = curl_init();
+
+	//JNE/////////////////////////
+$curl = curl_init();
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://api.rajaongkir.com/starter/cost",
@@ -91,7 +98,9 @@ curl_setopt_array($curl, array(
   ),
 ));
 
-$response = curl_exec($curl);
+
+
+$response1 = curl_exec($curl);
 $err = curl_error($curl);
 
 curl_close($curl);
@@ -99,9 +108,54 @@ curl_close($curl);
 if ($err) {
   echo "cURL Error #:" . $err;
 } else {
-  echo $response;
+  $data = json_decode($response1, true);
+
+//print_r ($data);  
+for($j=0; $j < count($data['rajaongkir']['results'][0]['costs']); $j++){
+echo ($data['rajaongkir']['results'][0]['costs'][$j]['service']);
 }
+	
 }
+
+	//POS/////////////////////////////
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.rajaongkir.com/starter/cost",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "origin=256&destination=$destination&weight=1000&courier=pos",
+  CURLOPT_HTTPHEADER => array(
+    "content-type: application/x-www-form-urlencoded",
+    "key: 6a9589b574e020b90d72190d410437ff"
+  ),
+));
+
+
+
+$response2 = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  $data = json_decode($response2, true);
+
+//print_r ($data);  
+for($j=0; $j < count($data['rajaongkir']['results'][0]['costs']); $j++){
+echo ($data['rajaongkir']['results'][0]['costs'][$j]['service']);
+}
+	
+}
+
+}
+
 if (isset($_POST['city'])) {
         echo get_cost($_POST['city']);
     }
